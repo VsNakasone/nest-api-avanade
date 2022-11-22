f54095ceaca85ae54db3e934ecab07cccd1a632d
Vanessa Nakasone <vsnakasone@gmail.com>
2022-11-22 15:18:15 -0300

    auth

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   //A função bootstrap é o start da aplicação, ela sempre fica no main
   //NestFactory é um design pattern
   const app = await NestFactory.create(AppModule);
+
+  app.use(json());
+  app.use(urlencoded({ extended: true}));
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,10 +3,28 @@ import { users } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateUserDTO } from './dto/createUser.dto';
 import { UpdateUsertDTO } from './dto/updateUser.dto';
+import *  as bcrypt from 'bcrypt' ;
+import { EmailService } from 'src/email/email.service';
+import { userInfo } from 'os';
 
 @Injectable()
 export class UsersService {
-    constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private emailService: EmailService,) {}
+
+async getUserById(id:string): Promise<users>{
+    const user = await this.prisma.users.findUnique({
+        where: {
+            id: Number(id)
+        },
+    });
+    if (!user) {
+        throw new HttpException('Usuário não encontrado', HttpStatus.NOT_FOUND);
+    }
+    return user;
+}
+
     async verifyUserExists(email: string): Promise<boolean>{
         const user = await this.prisma.users.findUnique({
             where: {
@@ -17,7 +35,14 @@ export class UsersService {
         return user ? true : false;
     }
 
+    async crypto(password: string): Promise<string> {
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        return hashedPassword;
+    }
     async create(data: CreateUserDTO): Promise<users>{
+//a const abaixo é uma atribuição por desestruturação        
         const {name, email, password} = data;
 //busca pra saber se o uruário já existe.
 //findUnique é um método do prisma que busca um usuário pelo campo único, por exemplo.
@@ -29,13 +54,18 @@ const checkUser = await this.verifyUserExists(email);
 let user = undefined;
 
 if(!checkUser){
-    const user = await this.prisma.users.create({
+     user = await this.prisma.users.create({
         data: {
             name,
             email,
-            password,
+            password: await this.crypto(password),
         },
     });
+    //enviando email
+    if (await this.emailService.sendEmail(email, 'Bem vindo ao sistema','Você se cadastrou no site Fiap Avanade', {}))
+    {
+        console.log('Email enviado com sucesso!');
+    }
 }
         
         if (!user) {
@@ -58,10 +88,67 @@ if(!checkUser){
             },
         });
     }
-    async update(id: number, req: UpdateUsertDTO): Promise<string>{
-        return ` Usuário ${id} atualizado com sucesso!` ;
+    async update(id: number, req: UpdateUsertDTO): Promise<object>{
+//essa variável user são os dados atualizados
+       const user = await this.getUserById(id.toString());
+       const{name, email, password} = req;
+//antes de alterar alguém no banco, é necessário verificar se o email desejado está disponível
+
+        if(email){
+            const checkEmail = await this.prisma.users.findMany({
+                where: {
+                 AND: [{email: email} ,{id: {not: Number(id)}}],
+                },
+            });
+            if (checkEmail.length > 0) {
+                throw new HttpException(
+                    {
+                        status: HttpStatus.FORBIDDEN,
+                        message: 'Este e-mail está indisponível!',
+                    },
+                    HttpStatus.FORBIDDEN,
+                );
+            }
+        }
+       const updatedUser = await this.prisma.users.update({
+        where:{
+            id: Number(id),
+        },
+        data: {
+            name: name ? name : user.name,
+            email: email ? email : user.email,
+            password: password ? await  this.crypto(password) : user.password,
+        },
+       });
+      
+       if (!updatedUser) {
+        throw new HttpException(
+            {
+                status: HttpStatus.FORBIDDEN,
+                message: 'Este e-mail está indisponível!',
+            },
+            HttpStatus.FORBIDDEN,
+        );
+       }
+       return { msg: `Usuário ${updatedUser.name} atualizado com sucesso!` };
     }
-    async remove(id: number) : Promise<string> {
-        return `Usuário ${id} deletado com sucesso!`;
+
+    async remove(id: number) : Promise<object> {
+        const user= await this.getUserById(id.toString());
+        const deletedUser = await this.prisma.users.delete({
+            where: {
+                id: Number(id),
+            },
+        });
+        if (!deletedUser) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.FORBIDDEN,
+                    message: 'Erro ao deletar usuário!',
+                },
+                HttpStatus.FORBIDDEN,
+            );
+        }
+        return{msg: `Usuário ${user.name} deletado com sucesso!`};
     }
 }
